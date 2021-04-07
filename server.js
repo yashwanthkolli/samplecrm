c1226904d117388f69f20f8d909dfc23d4544c7d
Devendra Pratap Singh <ashudev3254@gmail.com>
2021-04-07 15:21:04 +0530

    Added Configuration

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
+require('dotenv').config({
+    path:'./config/config.env'
+});
+
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
 
 const app = express();
-
-//import the routes that you want to use for specific routes.
 
 app.use(express.json())
 app.use(cors({
     origin: "http://localhost:3000"
 }))
 
-app.listen(3001, (req, res) => {
+app.listen(process.env.PORT, (req, res) => {
     console.log("Server Listening At Port 3001");
 })

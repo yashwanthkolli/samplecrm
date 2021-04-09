85fddcf7085da6fdccccd837db08e4443ba3dc7e
Devendra Pratap Singh <ashudev3254@gmail.com>
2021-04-09 18:16:04 +0530

    LoginPage-1

--- client/src/Components/Login/LoginComponents.js
+++ client/src/Components/Login/LoginComponents.js
@@ -1,3 +1,33 @@
 import styled from 'styled-components';
 
-export const Section = styled.div``
+export const Section = styled.div`
+    height: 100vh;
+    display: grid;
+    place-items: center;
+    background-color: #E0E0F8;
+`
+export const Holder = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 50%;
+    height: 425px;
+    background-color: white;
+`
+
+export const Container = styled.div`
+    display: grid;
+    width: 100%;
+    height: 100%;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 5px;
+`
+
+export const Left = styled.div``
+
+export const Logo = styled.img`
+    width: 100%;
+    height: 100%;
+`
+
+export const Right = styled.div``
--- client/src/Components/Login/index.js
+++ client/src/Components/Login/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
 import {
-    Section
+    Section,
+    Holder,
+    Container,
+    Left,
+    Right,
+    Logo
 } from './LoginComponents.js';
+const ice = require('../../images/001.png').default;
 
 function Login() {
 
     return (
         <Section>
-            Home
+            <Holder>
+                <Paper elevation={5} variant="elevation" style={{width: "100%", height: "100%"}}>
+                    <Container>
+                        <Left>
+                            <Logo src={ice} />
+                        </Left>
+                        <Right>
+                        </Right>
+                    </Container>
+                </Paper>
+            </Holder>
         </Section>
     )
 }

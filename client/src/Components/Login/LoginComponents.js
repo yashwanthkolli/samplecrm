3395fadf9f2ba3d211e21aced188e4454e59f4e5
Devendra Pratap Singh <ashudev3254@gmail.com>
2021-04-09 19:12:10 +0530

    Added Login Styles

--- client/src/Components/Login/LoginComponents.js
+++ client/src/Components/Login/LoginComponents.js
@@ -19,15 +19,39 @@ export const Container = styled.div`
     display: grid;
     width: 100%;
     height: 100%;
-    grid-template-columns: 1fr 1fr;
+    grid-template-columns: 0.9fr 1.1fr;
     grid-gap: 5px;
 `
 
-export const Left = styled.div``
+export const Left = styled.div`
+    display: grid;
+    place-items: center;
+`
 
 export const Logo = styled.img`
     width: 100%;
     height: 100%;
 `
 
-export const Right = styled.div``
+export const Right = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 20px 0;
+    align-items: center;
+    justify-content: space-evenly;
+`
+
+export const Ice = styled.img`
+    width: 45%;
+    display: grid;
+    place-items: center;
+`
+
+export const Form = styled.div`
+    display: grid;
+    place-items: center;
+    width: 100%;
+    height: 100%;
+    padding: 10px 0px;
+    grid-template-rows: 1fr 1fr 1fr;
+`
--- client/src/Components/Login/index.js
+++ client/src/Components/Login/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import {
     Section,
     Holder,
     Container,
     Left,
     Right,
-    Logo
+    Logo,
+    Ice,
+    Form
 } from './LoginComponents.js';
+
 const ice = require('../../images/001.png').default;
+const signIn = require('../../images/signIn.svg').default;
 
 function Login() {
+
+    const handleSubmit = (e) => {
+
+    }
 
     return (
         <Section>
@@ -18,9 +28,33 @@ function Login() {
                 <Paper elevation={5} variant="elevation" style={{width: "100%", height: "100%"}}>
                     <Container>
                         <Left>
-                            <Logo src={ice} />
+                            <Logo src={signIn} />
                         </Left>
                         <Right>
+                            <Ice src={ice} />
+                            <form onSubmit={handleSubmit} style={{width: "90%"}}>
+                                <Form>
+                                    <TextField
+                                        fullWidth
+                                        name="email"
+                                        type="email"
+                                        label="Email"
+                                        required
+                                        autoComplete="off"
+                                    />
+                                    <TextField
+                                        fullWidth
+                                        name="password"
+                                        type="password"
+                                        label="Password"
+                                        required
+                                        autoComplete="off"
+                                    />
+                                    <Button color="primary" variant="contained" type="submit">
+                                        Log In
+                                    </Button>
+                                </Form>
+                            </form>
                         </Right>
                     </Container>
                 </Paper>

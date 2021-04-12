d72105b5d300a80fd1ba95d3224832b2aa7de728
Devendra Pratap Singh <ashudev3254@gmail.com>
2021-04-12 20:23:38 +0530

    Added Holder Drawer

--- client/src/Components/CRM/Holder/index.js
+++ client/src/Components/CRM/Holder/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 import {ImProfile} from 'react-icons/im';
 import {BsFillChatSquareDotsFill} from 'react-icons/bs';
 import { FiLogOut } from 'react-icons/fi';
 import {BiCarousel} from 'react-icons/bi';
 import { AiFillHome } from 'react-icons/ai';
 import {MdEventNote, MdFeedback, MdUpdate} from 'react-icons/md';
+import { SiGoogleanalytics } from 'react-icons/si';
 import {Link} from 'react-router-dom';
 import { signout } from '../../../helpers/auth.helpers';
 import { FaUserEdit, FaUserFriends } from 'react-icons/fa';
 import {TiTick} from 'react-icons/ti';
 import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
@@ -80,155 +82,191 @@ const useStyles = makeStyles((theme) => ({
   toolbar: {
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'flex-end',
+    width: '100%',
+    justifyContent: 'space-around',
     padding: theme.spacing(0, 1),
     ...theme.mixins.toolbar,
   },
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
+    backgroundColor: '#E0E0F8',
+    zIndex: '5'
   },
   navbarSpace: {
     display: 'flex',
     width: '100%',
     alignItems: 'center',
     justifyContent: 'space-between'
+  },
+  title: {
+    fontFamily: 'Nunito',
+    fontSize: '20px'
+  },
+  iconCenter: {
+    display: 'flex',
+    paddingLeft: theme.spacing(1)
+  },
+  listStyle: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    minHeight: '65%',
+    zIndex: '10'
+  },
+  list: {
+    width: 225,
+  },
+  fullList: {
+    width: 'auto',
   }
 }));
 
 function Holder({match, navigate}){
 
-    const classes = useStyles();
-    const theme = useTheme();
-    const [open, setOpen] = React.useState(false);
+  const classes = useStyles();
+  const theme = useTheme();
+  const open = false;
+  const [state, setState] = useState({
+    left: false
+  });
 
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
-
-    const handleDrawerClose = () => {
-        setOpen(false);
-    };
-
-    const adminNav = [
-      {id: 1 ,path: `${match.path}/profile`,  icon: ImProfile },
-      {id: 2 ,path: `${match.path}/userAdmin`,  icon: FaUserEdit },
-      {id: 4 ,path: `${match.path}/eventAdmin`,  icon: BiCarousel },
-      {id: 5 ,path: `${match.path}/updateAdmin`,  icon: MdUpdate },
-      {id: 6 ,path: `${match.path}/approval`,  icon: TiTick }
-    ]
-
-    const teamNav = [
-      {id: 1, path: `${match.path}/profile`,  icon: ImProfile },
-      {id: 2, path: `${match.path}/userTeam`,  icon: FaUserFriends },
-      {id: 3, path: `${match.path}/eventTeam`,  icon: MdEventNote },
-      {id: 4, path: `${match.path}/updateTeam`,  icon: MdUpdate },
-      {id: 5, path: `${match.path}/permission`, icon: TiTick }
-    ]
-
-    const userNav = [
-      {id: 6, path: `${match.path}/home`, icon: AiFillHome, text: 'Home'},
-      {id: 1, path: `${match.path}/profile`,  icon: ImProfile, text: 'Profile' },
-      {id: 2, path: `${match.path}/events`,  icon: BiCarousel, text: 'Profile' },
-      {id: 3, path: `${match.path}/updates`,  icon: MdUpdate, text: 'Profile' },
-      {id: 4, path: `${match.path}/contact`,  icon: BsFillChatSquareDotsFill, text: 'Profile' },
-      {id: 5, path: `${match.path}/feedback`,  icon: MdFeedback, text: 'Profile' }
-    ]
-
-    let navbarElements;
-
-    switch(JSON.parse(localStorage.getItem('user')).Type){
-      case 'admin':
-        navbarElements = adminNav
-        break;
-      case 'nationalHead':
-        navbarElements = teamNav
-        break;
-      case 'ACCTS':
-        navbarElements = userNav
-        break;
-      default:
-        break;
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
     }
-    return(
-      <div className = {classes.root}>
-        <CssBaseline />
-        <AppBar
-          position="fixed"
-          className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
-          })}
-        >
-          <Toolbar>
+    setState({ ...state, [anchor]: open });
+  };
+
+
+  const adminNav = [
+    {id: 1 ,path: `${match.path}/profile`,  icon: ImProfile },
+    {id: 2 ,path: `${match.path}/userAdmin`,  icon: FaUserEdit },
+    {id: 4 ,path: `${match.path}/eventAdmin`,  icon: BiCarousel },
+    {id: 5 ,path: `${match.path}/updateAdmin`,  icon: MdUpdate },
+    {id: 6 ,path: `${match.path}/approval`,  icon: TiTick }
+  ]
+
+  const teamNav = [
+    {id: 1, path: `${match.path}/profile`,  icon: ImProfile },
+    {id: 2, path: `${match.path}/userTeam`,  icon: FaUserFriends },
+    {id: 3, path: `${match.path}/eventTeam`,  icon: MdEventNote },
+    {id: 4, path: `${match.path}/updateTeam`,  icon: MdUpdate },
+    {id: 5, path: `${match.path}/permission`, icon: TiTick }
+  ]
+
+  const userNav = [
+    {id: 6, path: `${match.path}/home`, icon: AiFillHome, text: 'Home'},
+    {id: 1, path: `${match.path}/profile`,  icon: ImProfile, text: 'Profile' },
+    {id: 2, path: `${match.path}/events`,  icon: BiCarousel, text: 'Profile' },
+    {id: 3, path: `${match.path}/updates`,  icon: MdUpdate, text: 'Profile' },
+    {id: 4, path: `${match.path}/contact`,  icon: BsFillChatSquareDotsFill, text: 'Profile' },
+    {id: 5, path: `${match.path}/feedback`,  icon: MdFeedback, text: 'Profile' }
+  ]
+
+  let navbarElements;
+
+  switch(JSON.parse(localStorage.getItem('user')).Type){
+    case 'admin':
+      navbarElements = adminNav
+      break;
+    case 'nationalHead':
+      navbarElements = teamNav
+      break;
+    case 'ACCTS':
+      navbarElements = userNav
+      break;
+    default:
+      break;
+  }
+
+  const list = (anchor) => (
+    <div
+      className={clsx(classes.list)}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List className={classes.listStyle}>
+      {navbarElements.map((element) => (
+        <Link to={element.path} key={element.id}>
+          <ListItem button >
+          <ListItemIcon className={classes.iconCenter}>
+            <Icon component={element.icon} />
+          </ListItemIcon>
+          <ListItemText primary={element.text} style={{fontFamily: 'Nunito !important', fontSize: '16px !important'}}/>
+          </ListItem>
+        </Link>
+      ))}
+      </List>
+      <Divider />
+    </div>
+  );
+
+  return(
+    <div className = {classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+        [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={toggleDrawer("left", true)}
+            edge="start"
+            className={clsx(classes.menuButton, {
+            [classes.hide]: open,
+            })}
+          >
+            <MenuIcon />
+          </IconButton>
+          <div className={classes.navbarSpace}>
+            <Typography variant="h6" noWrap style={{fontFamily: 'Nunito'}}>
+              ICE CRM
+            </Typography>
             <IconButton
               color="inherit"
-              aria-label="open drawer"
-              onClick={handleDrawerOpen}
-              edge="start"
-              className={clsx(classes.menuButton, {
-              [classes.hide]: open,
-              })}
+              aria-label="Logout"
+              onClick={signout}
+              edge="end"
             >
-              <MenuIcon />
+              <Link to="/">
+                <FiLogOut />
+              </Link>
             </IconButton>
-            <div className={classes.navbarSpace}>
-              <Typography variant="h6" noWrap>
-                ICE Institute CRM
+          </div>
+        </Toolbar>
+      </AppBar>
+      <div>
+        <React.Fragment key={"left"}>
+          <SwipeableDrawer
+            anchor={"left"}
+            open={state["left"]}
+            onClose={toggleDrawer("left", false)}
+            onOpen={toggleDrawer("left", true)}
+          >
+            <div className={classes.toolbar}>
+              <Icon component={SiGoogleanalytics} />
+              <Typography variant="h6" noWrap className={classes.title}>
+                Dashboard
               </Typography>
-              <IconButton
-                color="inherit"
-                aria-label="Logout"
-                onClick={signout}
-                edge="end"
-              >
-                <Link to="/">
-                  <FiLogOut />
-                </Link>
-              </IconButton>
             </div>
-            
-          </Toolbar>
-        </AppBar>
-        <Drawer
-          variant="permanent"
-          className={clsx(classes.drawer, {
-          [classes.drawerOpen]: open,
-          [classes.drawerClose]: !open,
-          })}
-          classes={{
-          paper: clsx({
-              [classes.drawerOpen]: open,
-              [classes.drawerClose]: !open,
-          }),
-          }}
-        >
-          <div className={classes.toolbar}>
-            <IconButton onClick={handleDrawerClose}>
-              {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-            </IconButton>
-          </div>
-          <Divider />
-          <List>
-          {navbarElements.map((element) => (
-            <Link to={element.path} key={element.id}>
-              <ListItem button>
-              <ListItemIcon>
-                <Icon component={element.icon} />
-              </ListItemIcon>
-              <ListItemText primary={element.text} />
-              </ListItem>
-            </Link>
-          ))}
-          </List>
-          <Divider />
-        </Drawer>
-        <main className={classes.content}>
-          <div className={classes.toolbar}>
-            {navigate()}
-          </div>
-        </main>
+            <Divider />
+            {list("left")}
+          </SwipeableDrawer>
+        </React.Fragment>
       </div>
-    )
+      <main className={classes.content}>
+        <div className={classes.toolbar}>
+          {navigate()}
+        </div>
+      </main>
+    </div>
+  )
 }
 
 export default Holder;

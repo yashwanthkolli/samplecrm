08226e0f08daba30d253b01b7dff863256f9abdb
Devendra Pratap Singh <ashudev3254@gmail.com>
2021-04-14 02:46:34 +0530

    Backend Template For Adding Users

--- client/src/Components/CRM/AddUsers/index.js
+++ client/src/Components/CRM/AddUsers/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useToast } from '@chakra-ui/react';
+import MaterialTable from 'material-table';
 
 function AddUsers(){
 
+    const toast = useToast();
+    const [tableData, setTableData] = useState([]);
+
+    useEffect(() => {
+        axios.post(`${process.env.REACT_APP_USER}/addUsers`,{
+            email: JSON.parse(localStorage.getItem('user')).Email
+        })
+        .then((res) => {
+        })
+        .catch((err) => {})
+    })
+
     return (
         <>
-            
+
         </>
     )
 }

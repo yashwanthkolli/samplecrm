c10218b6fe0769fc36c45c0c54e36b32ec23990a
unknown <yashwanthkolli3271@gmail.com>
2021-04-22 15:14:57 +0530

    Home Page Graphs and Maps

--- client/src/Components/CRM/Home/index.js
+++ client/src/Components/CRM/Home/index.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import MapRepresentation from './MapRepresentation'
+import ReactTooltip from "react-tooltip";
+import LeadToppersGraph from './leadtoppers';
+import PlacewiseBookingsGraph from './PlacewiseBookings';
+import LastFifteenDaysGraph from './LastFifteenDays';
+import Sources from './Sources';
+import CoursesGraph from './Course';
+import './styles.css'
 
 function Home(){
-
     return(
-        <div>Home</div>
+        <div style={{background: '#E0E0F8', display: 'flex', justifyContent: 'center', padding: '50px 0'}}>
+            <div className='home-page'>
+                <div className='monthly-booking'>
+                    <PlacewiseBookingsGraph />
+                </div>
+
+                <div className='sources-chart'>
+                    <Sources />
+                </div>
+                
+                <div className='toppers-graph'>
+                    <LeadToppersGraph />
+                </div>
+
+                <div className='courses-chart'>
+                    <CoursesGraph />
+                </div>
+
+                <div className='past-leads-graph'>
+                    <LastFifteenDaysGraph />
+                </div>
+
+                <div className='leads-map'>
+                    <h1 className='map-heading'>Citywise Leads</h1>
+                    <MapRepresentation />
+                    <ReactTooltip />
+                </div>
+            </div>
+        </div>
     )
 }
 
